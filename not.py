a80f54d94345868e12031521a762e2896366539c
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2025-12-31 20:13:26 +0500

    Operator 2

--- not.py
+++ not.py
@@ -1,3 +1,18 @@
 a=int(input("Enter a number"))
 if a%2 !=0:
     print(a, "is not even number")
+a=10
+b=12
+c=12
+print(a != b)
+print(b !=c)
+
+a="python"
+b="codingal"
+if a!=b:
+    print(a,"and",b,"are diffirent")
+
+a=4
+b=5
+if (a==1)!=(b==5):
+    print("Happy New Year!")
