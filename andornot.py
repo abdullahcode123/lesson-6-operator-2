421175c176ac97b0181bcac9993fccd6cc2e23aa
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2025-12-31 20:07:21 +0500

    OpeaRator 2

---
+++ andornot.py
@@ -0,0 +1,11 @@
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No of the num is greator than0")
+if b>0 and c>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No of the num is greator than0")
