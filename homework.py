467227f224c5079718998191e318f5b86d64e4c0
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2025-12-31 20:27:14 +0500

    operator II

---
+++ homework.py
@@ -0,0 +1,5 @@
+ip=input("Enter any thing")
+
+
+if ip == "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "w" or "v" or "x" or "y" or "z":
+    print("It's an alphabet")
